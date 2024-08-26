91d2dfeb4136743b98f3af288b9f38d92e28de43
Muhammad Usman <musman.akhtar21@gmail.com>
2024-08-26 22:48:22 +0500

    renderr install

--- api/index.js
+++ api/index.js
@@ -7,17 +7,20 @@ import speakerRoutes from "./routes/speaker.route.js";
 import orderRoutes from "./routes/order.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log("MongoDB is connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+	.connect(process.env.MONGO)
+	.then(() => {
+		console.log("MongoDB is connected");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -26,14 +29,18 @@ app.use(cors());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+	console.log("Server is running on port 3000!");
 });
-
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/speaker", speakerRoutes);
 app.use("/api/order", orderRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // import Stripe from "stripe";
 // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
@@ -90,11 +97,11 @@ app.use("/api/order", orderRoutes);
 // });
 
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
+	const statusCode = err.statusCode || 500;
+	const message = err.message || "Internal Server Error";
+	res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	});
 });
