cf1ac9041734e1cf5953c04d680ecf9b613d4dd2
Muhammad Zain <mzain.akhtar7@gmail.com>
2024-08-26 12:41:40 +0500

    upload

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -5,160 +5,178 @@ import Speaker from "../models/speaker.model.js";
 dotenv.config();
 
 export const getOrders = async (req, res) => {
-	try {
-		const userId = req.user.id;
-		const speakerId = await Speaker.find({ userId });
+  try {
+    const userId = req.user.id;
+    const speakerId = await Speaker.find({ userId });
 
-		const orders = await Order.find({
-			$or: [{ userId }, { speakerId }],
-		})
-			.populate({
-				path: "speakerId",
-				populate: {
-					path: "userId",
-					model: "User",
-				},
-			})
-			.populate("userId")
-			.sort({ updatedAt: -1 });
+    const orders = await Order.find({
+      $or: [{ userId }, { speakerId }],
+    })
+      .populate({
+        path: "speakerId",
+        populate: {
+          path: "userId",
+          model: "User",
+        },
+      })
+      .populate("userId")
+      .sort({ updatedAt: -1 });
 
-		res.status(200).json(orders);
-	} catch (error) {
-		res.status(500).json({ message: "Failed to fetch orders", error });
-	}
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch orders", error });
+  }
 };
 
 export const getOrderNotification = async (req, res) => {
-	try {
-		const userId = req.user.id;
-		const speakerId = await Speaker.find({ userId });
+  try {
+    const userId = req.user.id;
+    const speakerId = await Speaker.find({ userId });
 
-		const orderInfo = await Order.find(
-			{
-				$or: [{ userId }, { speakerId }],
-			},
-			"_id status updatedAt"
-		);
+    const orderInfo = await Order.find(
+      {
+        $or: [{ userId }, { speakerId }],
+      },
+      "_id status updatedAt"
+    );
 
-		res.status(200).json(orderInfo);
-	} catch (error) {
-		console.error("Error retrieving order status info:", error);
-		res.status(500).json({ message: "Server Error" });
-	}
+    res.status(200).json(orderInfo);
+  } catch (error) {
+    console.error("Error retrieving order status info:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 export const createNewOrder = async (req, res, next) => {
-	const data = req.body;
-	if (req.user.id !== data.user._id) {
-		return res.status(403).send({ error: "Unauthorized" });
-	}
+  const data = req.body;
+  if (req.user.id !== data.user._id) {
+    return res.status(403).send({ error: "Unauthorized" });
+  }
 
-	if (!data.user || !data.speaker || !data.data || !data.amount) {
-		return res.status(400).send({ error: "Invalid order data" });
-	}
+  if (!data.user || !data.speaker || !data.data || !data.amount) {
+    return res.status(400).send({ error: "Invalid order data" });
+  }
 
-	const duration = {
-		small: "10 ~ 20 seconds",
-		medium: "30 ~ 40 seconds",
-		large: "1 minute",
-	};
+  const duration = {
+    small: "10 ~ 20 seconds",
+    medium: "30 ~ 40 seconds",
+    large: "1 minute",
+  };
 
-	const order = new Order({
-		userId: data.user._id,
-		speakerId: data.speaker._id,
-		userPhone: data.data.number,
-		userComapny: data.data.company,
-		userContry: data.data.country,
-		userCity: data.data.city,
-		service: data.data.service,
-		audioDuration: data.data.duration,
-		specs: data.data.specs,
-		amount: data.amount,
-		status: "Pending Payment",
-	});
-	await order.save();
+  const order = new Order({
+    userId: data.user._id,
+    speakerId: data.speaker._id,
+    userPhone: data.data.number,
+    userComapny: data.data.company,
+    userContry: data.data.country,
+    userCity: data.data.city,
+    service: data.data.service,
+    audioDuration: data.data.duration,
+    specs: data.data.specs,
+    amount: data.amount,
+    status: "Pending Payment",
+  });
+  await order.save();
 
-	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-	const session = await stripe.checkout.sessions.create({
-		payment_method_types: ["card"],
-		line_items: [
-			{
-				price_data: {
-					currency: "usd",
-					product_data: {
-						name: `${data.data.service} (${data.speaker.gender} voiceover)`,
-						description: `${duration[data.data.duration]} duration by ${
-							data.speaker.userId.name
-						} from ${data.speaker.country}`,
-						images: [data.speaker.image],
-					},
-					unit_amount: data.amount * 100,
-				},
-				quantity: 1,
-			},
-		],
-		mode: "payment",
-		success_url: `${process.env.CLIENT_URL}/api/order/status?orderId=${order._id}&status=Pending Delivery`,
-		cancel_url: `${process.env.CLIENT_URL}/api/order/status?orderId=${order._id}&status=Cancelled`,
-	});
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: `${data.data.service} (${data.speaker.gender} voiceover)`,
+            description: `${duration[data.data.duration]} duration by ${
+              data.speaker.userId.name
+            } from ${data.speaker.country}`,
+            images: [data.speaker.image],
+          },
+          unit_amount: data.amount * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${process.env.CLIENT_URL}/api/order/status?orderId=${order._id}&status=Pending Delivery`,
+    cancel_url: `${process.env.CLIENT_URL}/api/order/status?orderId=${order._id}&status=Cancelled`,
+  });
 
-	res.status(200).json(session);
+  res.status(200).json(session);
 };
 
 export const updateOrderStatus = async (req, res) => {
-	const { orderId, status } = req.query;
-	const { audioFile, rejectMessage, speakerMessage } = req.body;
+  const { orderId, status } = req.query;
+  const { audioFile, rejectMessage, speakerMessage } = req.body;
 
-	try {
-		const order = await Order.findById(orderId);
-		if (!order) {
-			return res.status(404).send({ error: "Order not found" });
-		}
+  try {
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).send({ error: "Order not found" });
+    }
 
-		order.status = status;
-		order.audioFile = audioFile;
-		order.rejectMessage = rejectMessage;
-		order.speakerMessage = speakerMessage;
-		await order.save();
+    order.status = status;
+    order.audioFile = audioFile;
+    order.rejectMessage = rejectMessage;
+    order.speakerMessage = speakerMessage;
+    await order.save();
 
-		res.redirect("http://localhost:5173/orders");
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
+	res.status(200).send({ message: "Order status updated" });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+export const updateOrderPaymentStatus = async (req, res) => {
+  const { orderId, status } = req.query;
+
+  try {
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).send({ error: "Order not found" });
+    }
+
+    order.status = status;
+    await order.save();
+
+    res.redirect("http://localhost:5173/orders");
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
 
 export const orderDelivered = async (req, res) => {
-	const { orderId, speakerStripeAccountId } = req.body;
+  const { orderId, speakerStripeAccountId } = req.body;
 
-	try {
-		// Find the order by ID
-		const order = await Order.findById(orderId);
-		if (!order) {
-			return res.status(404).send({ error: "Order not found" });
-		}
+  try {
+    // Find the order by ID
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).send({ error: "Order not found" });
+    }
 
-		if (order.status !== "Pending Delivery") {
-			return res.status(400).send({ error: "Order is not pending delivery" });
-		}
+    if (order.status !== "Pending Delivery") {
+      return res.status(400).send({ error: "Order is not pending delivery" });
+    }
 
-		order.status = "Completed";
-		await order.save();
+    order.status = "Completed";
+    await order.save();
 
-		// Transfer funds to the speaker's Stripe-connected account
-		const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+    // Transfer funds to the speaker's Stripe-connected account
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-		const transfer = await stripe.transfers.create({
-			amount: order.amount * 100, // amount in cents
-			currency: "usd",
-			destination: speakerStripeAccountId,
-			transfer_group: `ORDER_${order._id}`,
-		});
+    const transfer = await stripe.transfers.create({
+      amount: order.amount * 100, // amount in cents
+      currency: "usd",
+      destination: speakerStripeAccountId,
+      transfer_group: `ORDER_${order._id}`,
+    });
 
-		res
-			.status(200)
-			.send({ message: "Order delivered and funds transferred", transfer });
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
+    res
+      .status(200)
+      .send({ message: "Order delivered and funds transferred", transfer });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { updateOrderStatus, createNewOrder, getOrders, getOrderNotification } from "../controllers/order.controller.js";
+import { updateOrderStatus, createNewOrder, getOrders, getOrderNotification, updateOrderPaymentStatus } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 router.get("/orders", verifyToken, getOrders);
 router.post("/create-new-order", verifyToken, createNewOrder);
 router.post("/status", verifyToken, updateOrderStatus);
+router.get("/status", verifyToken, updateOrderPaymentStatus);
 router.get("/orders-notifications", verifyToken, getOrderNotification);
 
 export default router;
