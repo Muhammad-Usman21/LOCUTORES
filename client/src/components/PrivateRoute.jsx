525bd71598f69b850a133a451c4cd31f5e876672
Muhammad Zain <mzain.akhtar7@gmail.com>
2024-08-27 04:49:10 +0500

    notification added

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,9 +1,17 @@
 import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const PrivateRoute = () => {
 	const { currentUser } = useSelector((state) => state.user);
-	return currentUser ? <Outlet /> : <Navigate to="sign-in" />;
+
+	if (!currentUser) {
+		toast.error("Please sign in to continue.");
+		return <Navigate to="sign-in" />;
+	}
+
+	return <Outlet />;
 };
 
 export default PrivateRoute;
