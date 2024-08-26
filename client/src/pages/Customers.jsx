1087597475af243a2799a34e284c5dc25d998075
Muhammad Zain <mzain.akhtar7@gmail.com>
2024-08-26 20:23:58 +0500

    debug

--- client/src/pages/Customers.jsx
+++ client/src/pages/Customers.jsx
@@ -6,22 +6,8 @@ const Customers = () => {
   }
     , []);
 
-  const func = async () => {
 
-    const data = {
-      speaker: {
-        _id: "123456"
-      }
-    };
-    const response = await fetch(`/api/auth/signin-stripe?speakerId=${data.speaker._id}`);
-    const result = await response.json();
-    console.log(result);
-    window.location.href = result.url;
-  }
-
-  return <div><button onClick={
-    func
-  }>click</button></div>;
+  return <div></div>;
 };
 
 export default Customers;
