ea3f0ce01a4711912120b90b81fa73be3e3ff326
Muhammad Zain <mzain.akhtar3@gmail.com>
2024-09-16 21:18:48 +0500

    error fixed

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { updateOrderStatus, createNewOrder, getOrders, getOrderNotification, updateOrderPaymentStatus } from "../controllers/order.controller.js";
+import { updateOrderStatus, createNewOrder, getOrders, getOrderNotification } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
